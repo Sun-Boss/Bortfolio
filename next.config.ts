ca130c18484482257a0551aebba13b08a578ef7e
Sun Boss <tjsqhk1@daum.net>
2026-03-26 19:27:30 +0900

    error 수정

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? '/Bortfolio' : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
